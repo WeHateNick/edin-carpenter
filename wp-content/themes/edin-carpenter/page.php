a339a6545c893eb0e1f5112327bebb2bee177c1b
Nick Ocampo <nick@wehatenick.com>
2014-07-02 00:18:39 -0700

    Properly linked to logo for sidebar in page.php

--- wp-content/themes/edin-carpenter/page.php
+++ wp-content/themes/edin-carpenter/page.php
@@ -4,17 +4,12 @@
       <!-- Sidebar
       ================================================== -->
       <div class="col-sm-3 hidden-xs sidebar">
-<!--           <ul class="sidebar-nav brackets-effect">
-              <li class="section"><a href="#">About</a></li>
-              <li class="active-page"><a href=".blog.html">Blog</a></li>
-              <li><a href="#">Testimonials</a></li>
-          </ul> -->
         <ul class="clearfix sidebar-nav brackets-effect">
           <?php wp_list_pages( array('title_li'=>'','include'=>get_post_top_ancestor_id()) ); ?>
           <?php wp_list_pages( array('title_li'=>'','depth'=>1,'child_of'=>get_post_top_ancestor_id()) ); ?>
         </ul>
 
-        <img src="wp-content/themes/edin-carpenter/img/ecma-logo-sidebar.png">
+        <img src="<?php echo get_template_directory_uri() . '/img/ecma-logo-sidebar.png';?>">
         <hr>
         <div class="social-icons underline-effect">
           <a href="#"><span class="icon-pinterest"></span></a>
