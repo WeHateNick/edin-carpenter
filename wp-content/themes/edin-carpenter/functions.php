a41470a08172715c0dc1e3f7af11aa25fa63222d
Nick Ocampo <nick@wehatenick.com>
2014-07-01 22:47:52 -0700

    Finished side navigation

--- wp-content/themes/edin-carpenter/functions.php
+++ wp-content/themes/edin-carpenter/functions.php
@@ -67,7 +67,24 @@ add_action('init', 'register_theme_menus');
 
 //             add_action('widgets_init', 'create_widget');
 
-
+if(!function_exists('get_post_top_ancestor_id')){
+/**
+ * Gets the id of the topmost ancestor of the current page. Returns the current
+ * page's id if there is no parent.
+ * 
+ * @uses object $post
+ * @return int 
+ */
+function get_post_top_ancestor_id(){
+    global $post;
+    
+    if($post->post_parent){
+        $ancestors = array_reverse(get_post_ancestors($post->ID));
+        return $ancestors[0];
+    }
+    
+    return $post->ID;
+}}
 
 
 
