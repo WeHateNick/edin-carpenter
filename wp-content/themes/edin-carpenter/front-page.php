25b824a07855771358a32715b687c3ca68a69549
Nick Ocampo <nick@wehatenick.com>
2014-07-12 20:14:38 -0700

    Finished Image slider

--- wp-content/themes/edin-carpenter/front-page.php
+++ wp-content/themes/edin-carpenter/front-page.php
@@ -12,51 +12,8 @@
       ?>
       <div class="col-sm-9">
         <div class="container-fluid">
-          <!-- Hero Shot
-          ================================================== -->
-          <div class="row">
-            <div class="col-xs-12 hero-shot"> 
-              <div id="carousel-work" class="carousel slide">
-                  <!-- Indicators -->
-                  <ol class="carousel-indicators">
-                    <li data-target="#carousel-work" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-work" data-slide-to="1"></li>
-                    <li data-target="#carousel-work" data-slide-to="2"></li>
-                  </ol>
-
-                  <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                           <!--  <div class="item active">
-                              <img src="img/crazy-eyes.png">
-                            </div> -->
-                          <?php 
-
-                            $args = array(
-                              'post_type' => 'slide',
-                              'posts_per_page' => 6
-                            );
-
-                            $the_query = new WP_Query($args);
-
-                          ?>
-
-                          <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <div class="item">
-                              <img src="<?php the_field('image'); ?>">
-                            </div>
-                          <?php endwhile; endif; ?>
-                        </div>
-                  <!-- Controls -->
-                  <a class="left carousel-control" href="#carousel-work" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                  </a>
-                  <a class="right carousel-control" href="#carousel-work" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                  </a>
-                </div> <!-- END CAROUSSELL  -->
-            </div>
-          </div> <!-- End Hero Shot -->
           <?php
+            get_template_part('content', 'slides');
             get_template_part('content', 'testimonial')
           ?>
         </div>  <!-- End container -->
